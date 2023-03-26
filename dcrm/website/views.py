8b4949ba801be3d4fff9c2ad201d6f8b76c1a3d5
Doheera-kosi <kupourevans99@gmail.com>
2023-03-26 22:08:11 +0000

    Modify views.py

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-
+from .forms import SignUpForm
 
 # Create your views here.
 def home(request):
@@ -31,4 +31,21 @@ def logout_user(request):
     return redirect('home')
 
 def register_user(request):
-    return render(request, 'register.html', {})
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticate and Login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "Welcome, Registered Successfully ")
+            return redirect('home')
+        
+    else:
+        form = SignUpForm()
+
+        return render(request, 'register.html', {'form':form})
+    
+    return render(request, 'register.html', {'form':form})
