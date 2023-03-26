0f0485099720afc7b7e41fe3cbb76c9b46aad81c
Doheera-kosi <kupourevans99@gmail.com>
2023-03-26 22:49:19 +0000

    Modify views.py and home.html

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
 
 # Create your views here.
 def home(request):
+    records = Record.objects.all()
+
     # Check to see if loggin in
     if request.method == 'POST':
         username = request.POST['username']
@@ -21,7 +24,7 @@ def home(request):
             messages.success(request, "Invalid Credentials, Try Again...")
         return redirect('home')
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records': records})
 
 
 
