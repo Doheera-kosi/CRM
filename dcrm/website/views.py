06bf61d22e9055e6f31641c48a2d945d0cd6edc4
Doheera-kosi <kupourevans99@gmail.com>
2023-03-26 02:00:30 +0000

    Add register.html and modify views, and urls.py

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -29,3 +29,6 @@ def logout_user(request):
     logout(request)
     messages.success(request, "Logged Out Successfully")
     return redirect('home')
+
+def register_user(request):
+    return render(request, 'register.html', {})
