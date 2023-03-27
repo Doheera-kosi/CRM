a61b30b0aa50c3b399aae9c20040e3b8d4074383
Doheera-kosi <kupourevans99@gmail.com>
2023-03-27 22:40:54 +0000

    Add add_records methods

--- dcrm/website/urls.py
+++ dcrm/website/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('register/', views.register_user, name='register'),
     path('record/<int:pk>', views.customer_record, name='record'),
     path('delete_record/<int:pk>', views.delete_record, name='delete_record'),
+    path('add_record/', views.add_record, name='add_record'),
 ]
--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm, AddRecordForm
 from .models import Record
 
 # Create your views here.
@@ -69,4 +69,18 @@ def delete_record(request, pk):
         return redirect('home')
     else:
         messages.success(request, "Log in to delete...")
+        return redirect('home')
+
+def add_record(request):
+    form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            if form.is_valid():
+                add_record = form.save()
+                messages.success(request, "Record Added...")
+                return redirect('home')
+            
+        return render(request, 'add_record.html', {'form': form})
+    else:
+        messages.success(request, "Login to proceed")
         return redirect('home')
